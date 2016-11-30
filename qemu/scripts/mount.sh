4907bf3aa3796ea4d3f962c3e1a88a82f3edd996
niorehkids <forendef2846@gmail.com>
2016-12-01 03:13:44 +0800

    add scripts for qemu vm

---
+++ qemu/scripts/mount.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -e
+set -u
+
+if [ -e ../../configure.sh ]; then
+    source ../../configure.sh
+elif [ -e ../configure.sh ]; then
+    source ../configure.sh
+else
+    echo "Error: Could not find 'configure.sh'!"
+    exit 1
+fi
+
+if check_number $1; then
+    echo "Usage: mount.sh <image ID>"
+    exit 1
+fi
+IID=${1}
+
+if check_root; then
+    echo "Error: This script requires root privileges!"
+    exit 1
+fi
+
+echo "----Running----"
+WORK_DIR=`get_vm ${IID}`
+IMAGE=`get_fs ${IID}`
+IMAGE_DIR=`get_fs_mount ${IID}`
+
+DEVICE=/dev/mapper/loop0p1
+
+echo "----Adding Device File----"
+#/usr/bin/qemu-nbd --connect=/dev/${NBD} "${IMAGE}"
+kpartx -a -v "${IMAGE}"
+sleep 1
+
+echo "----Making image directory----"
+if [ ! -e "${IMAGE_DIR}" ]
+then
+        mkdir "${IMAGE_DIR}"
+fi
+
+echo "----Mounting----"
+#sudo /bin/mount /dev/nbd0p1 "${IMAGE_DIR}"
+mount "${DEVICE}" "${IMAGE_DIR}"
---
+++ qemu/scripts/umount.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+set -u
+
+if [ -e ../../configure.sh ]; then
+    source ../../configure.sh
+elif [ -e ../configure.sh ]; then
+    source ../configure.sh
+else
+    echo "Error: Could not find 'configure.sh'!"
+    exit 1
+fi
+
+if check_number $1; then
+    echo "Usage: umount.sh <image ID>"
+    exit 1
+fi
+IID=${1}
+
+if check_root; then
+    echo "Error: This script requires root privileges!"
+    exit 1
+fi
+
+echo "----Running----"
+WORK_DIR=`get_vm ${IID}`
+IMAGE=`get_fs ${IID}`
+IMAGE_DIR=`get_fs_mount ${IID}`
+
+DEVICE=/dev/mapper/loop0p1
+
+echo "----Unmounting----"
+umount "${DEVICE}"
+
+echo "----Disconnecting Device File----"
+umount "${IMAGE}"
+kpartx -d "${IMAGE}"
+losetup -d "${DEVICE}" &>/dev/null
+dmsetup remove loop0p1 &>/dev/null
