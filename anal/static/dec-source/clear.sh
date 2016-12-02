21df0195c87ad27e1eb2a47c47f0ecfe23fdb6d3
niorehkids <forendef2846@gmail.com>
2016-12-02 11:30:44 +0800

    fix the condition

--- anal/static/dec-source/clear.sh
+++ anal/static/dec-source/clear.sh
@@ -25,9 +25,9 @@ WORK_DIR=${STATIC_DIR}/dec-source
 DEC_DIR=${WORK_DIR}/dec/${IID}
 OUT_DIR=${WORK_DIR}/out/${IID}
 
-if [[ ! -d ${DEC_DIR} ]]; then
+if [[ -d ${DEC_DIR} ]]; then
 	rm -rf ${DEC_DIR}
 fi
-if [[ ! -d ${OUT_DIR} ]]; then
+if [[ -d ${OUT_DIR} ]]; then
 	rm -rf ${OUT_DIR}
 fi
