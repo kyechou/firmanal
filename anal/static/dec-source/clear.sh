4a5961cfc2285fbfaaab6e123d487d4ca2a90e5e
niorehkids <forendef2846@gmail.com>
2016-12-02 11:28:59 +0800

    update the directories names

--- anal/static/dec-source/clear.sh
+++ anal/static/dec-source/clear.sh
@@ -15,23 +15,19 @@ else
 	exit 1
 fi
 
-if [ $# -ne 1 ]; then
+if [[ $# -ne 1 ]]; then
 	echo "Usage: $0 <image ID>"
 	exit 1
 fi
 
 IID=${1}
 WORK_DIR=${STATIC_DIR}/dec-source
-EXTRACT_DIR=${WORK_DIR}/extract/${IID}
 DEC_DIR=${WORK_DIR}/dec/${IID}
-RESULT_DIR=${WORK_DIR}/result/${IID}
+OUT_DIR=${WORK_DIR}/out/${IID}
 
-if [ -d ${EXTRACT_DIR} ]; then
-	rm -rf ${EXTRACT_DIR}
-fi
-if [ -d ${DEC_DIR} ]; then
+if [[ ! -d ${DEC_DIR} ]]; then
 	rm -rf ${DEC_DIR}
 fi
-if [ -d ${RESULT_DIR} ]; then
-	rm -rf ${RESULT_DIR}
+if [[ ! -d ${OUT_DIR} ]]; then
+	rm -rf ${OUT_DIR}
 fi
