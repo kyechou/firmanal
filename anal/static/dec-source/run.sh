3494717c4213a7bfca74b7be7bc3eed5a86d9967
kyechou <forendef2846@gmail.com>
2017-01-16 03:53:36 +0800

    add -f options to rm

--- anal/static/dec-source/run.sh
+++ anal/static/dec-source/run.sh
@@ -33,10 +33,10 @@ tar xf ${FIRMWARE_DIR}/${IID}.tar.gz -C ${OUT_DIR} $(psql -U firmadyne -d firmwa
 
 # decompile the executables
 echo "Decompiling binaries......"
-find ${OUT_DIR} -type f -exec bash -c 'nocode "$0" > "$0".dec.c; rm "$0"' {} \;
+find ${OUT_DIR} -type f -exec bash -c 'nocode "$0" > "$0".dec.c; rm -f "$0"' {} \;
 
 # use flawfinder to do the source-code static analysis
 echo "Static analysis using flawfinder......"
 cd ${OUT_DIR}	# in order not to show the full path in the output results
-find . -type f -name '*.dec.c' -exec bash -c 'flawfinder -cC "$0" > "$0".out; rm "$0"' {} \;
+find . -type f -name '*.dec.c' -exec bash -c 'flawfinder -cC "$0" > "$0".out; rm -f "$0"' {} \;
 cd -
