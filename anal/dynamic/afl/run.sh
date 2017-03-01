347133a6e63a3eeeb2505d95b36008288c9a0eee
kyechou <forendef2846@gmail.com>
2017-03-01 09:37:19 +0800

    handling signals for running afl

--- anal/dynamic/afl/run.sh
+++ anal/dynamic/afl/run.sh
@@ -48,20 +48,24 @@ if [[ -z "${AFL_PATH}" ]]; then
 	exit 1
 fi
 
+trap "" SIGINT
+trap "trap '' SIGTERM; kill -TERM 0; exit 0" SIGHUP SIGQUIT SIGTERM
+
 # fuzz the binaries
 for target in $(psql -U firmadyne -d firmware -c "select filename from object_to_image where iid=${IID} and score>0 and mime='application/x-executable; charset=binary' order by score DESC;" | tail -n+3 | head -n-2 | sed -e "s/^ \///")
 do
+	echo -e "\nfuzzing ${target}..."
 	if [[ ! -d ${OUT_DIR}/${target} ]]; then
 		mkdir -p ${OUT_DIR}/${target}
-		AFL_PATH=${AFL_PATH} afl-fuzz -Q -M master -m ${MEMORY} -i /usr/share/afl/testcases/others/text -o ${OUT_DIR}/${target} -L ${BIN_DIR}/ ${BIN_DIR}/${target}  &> /dev/null  &
-		AFL_PATH=${AFL_PATH} afl-fuzz -Q -S slave1 -m ${MEMORY} -i /usr/share/afl/testcases/others/text -o ${OUT_DIR}/${target} -L ${BIN_DIR}/ ${BIN_DIR}/${target}  &> /dev/null  &
-		AFL_PATH=${AFL_PATH} afl-fuzz -Q -S slave2 -m ${MEMORY} -i /usr/share/afl/testcases/others/text -o ${OUT_DIR}/${target} -L ${BIN_DIR}/ ${BIN_DIR}/${target}  &> /dev/null  &
-		AFL_PATH=${AFL_PATH} afl-fuzz -Q -S slave3 -m ${MEMORY} -i /usr/share/afl/testcases/others/text -o ${OUT_DIR}/${target} -L ${BIN_DIR}/ ${BIN_DIR}/${target}  &> /dev/null  &
+		AFL_PATH=${AFL_PATH} afl-fuzz -Q -M master -m ${MEMORY} -i /usr/share/afl/testcases/others/text -o ${OUT_DIR}/${target} -L ${BIN_DIR}/ ${BIN_DIR}/${target} &> /dev/null &
+		AFL_PATH=${AFL_PATH} afl-fuzz -Q -S slave1 -m ${MEMORY} -i /usr/share/afl/testcases/others/text -o ${OUT_DIR}/${target} -L ${BIN_DIR}/ ${BIN_DIR}/${target} &> /dev/null &
+		AFL_PATH=${AFL_PATH} afl-fuzz -Q -S slave2 -m ${MEMORY} -i /usr/share/afl/testcases/others/text -o ${OUT_DIR}/${target} -L ${BIN_DIR}/ ${BIN_DIR}/${target} &> /dev/null &
+		AFL_PATH=${AFL_PATH} afl-fuzz -Q -S slave3 -m ${MEMORY} -i /usr/share/afl/testcases/others/text -o ${OUT_DIR}/${target} -L ${BIN_DIR}/ ${BIN_DIR}/${target} &> /dev/null &
 	else
-		AFL_PATH=${AFL_PATH} afl-fuzz -Q -M master -m ${MEMORY} -i - -o ${OUT_DIR}/${target} -L ${BIN_DIR}/ ${BIN_DIR}/${target}  &> /dev/null  &
-		AFL_PATH=${AFL_PATH} afl-fuzz -Q -S slave1 -m ${MEMORY} -i - -o ${OUT_DIR}/${target} -L ${BIN_DIR}/ ${BIN_DIR}/${target}  &> /dev/null  &
-		AFL_PATH=${AFL_PATH} afl-fuzz -Q -S slave2 -m ${MEMORY} -i - -o ${OUT_DIR}/${target} -L ${BIN_DIR}/ ${BIN_DIR}/${target}  &> /dev/null  &
-		AFL_PATH=${AFL_PATH} afl-fuzz -Q -S slave3 -m ${MEMORY} -i - -o ${OUT_DIR}/${target} -L ${BIN_DIR}/ ${BIN_DIR}/${target}  &> /dev/null  &
+		AFL_PATH=${AFL_PATH} afl-fuzz -Q -M master -m ${MEMORY} -i - -o ${OUT_DIR}/${target} -L ${BIN_DIR}/ ${BIN_DIR}/${target} &> /dev/null &
+		AFL_PATH=${AFL_PATH} afl-fuzz -Q -S slave1 -m ${MEMORY} -i - -o ${OUT_DIR}/${target} -L ${BIN_DIR}/ ${BIN_DIR}/${target} &> /dev/null &
+		AFL_PATH=${AFL_PATH} afl-fuzz -Q -S slave2 -m ${MEMORY} -i - -o ${OUT_DIR}/${target} -L ${BIN_DIR}/ ${BIN_DIR}/${target} &> /dev/null &
+		AFL_PATH=${AFL_PATH} afl-fuzz -Q -S slave3 -m ${MEMORY} -i - -o ${OUT_DIR}/${target} -L ${BIN_DIR}/ ${BIN_DIR}/${target} &> /dev/null &
 	fi
 	wait
 done
